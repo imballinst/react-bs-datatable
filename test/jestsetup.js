5da2f4311a60b6855c2cd061558887618b568ddc
Try Ajitiono <ballinst@gmail.com>
2019-04-21 06:01:41 +1000

    Update snapshot, tests are now broken
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- test/jestsetup.js
+++ test/jestsetup.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {
-  shallow,
-  render,
-  mount
-} from 'enzyme';
+import { shallow, render, mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
+// Set these test-related things global so we don't have to re-import them in every package.
 global.React = React;
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
 
+// Configure Enzyme adapter.
+configure({ adapter: new Adapter() });
+
 // Fail tests on any warning
 console.error = message => {
-   throw new Error(message);
+  throw new Error(message);
 };
