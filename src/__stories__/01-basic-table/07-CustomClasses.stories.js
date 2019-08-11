4521e86a2826283e056ab0830c331702ecac31c6
Try Ajitiono <ballinst@gmail.com>
2019-08-12 09:25:47 +1000

    Add .babelrc to storybook folder, add CustomClasses story

---
+++ src/__stories__/01-basic-table/07-CustomClasses.stories.js
@@ -0,0 +1,91 @@
+import React from 'react'; // Import React
+import { storiesOf } from '@storybook/react';
+import { css } from 'emotion';
+
+import { categoryName } from './_base';
+
+import moment from 'moment'; // Example for onSort prop
+import Datatable from '../../Table'; // Import this package
+
+const header = [
+  {
+    title: 'Username (filterable)',
+    prop: 'username',
+    sortable: true,
+    filterable: true
+  },
+  { title: 'Name', prop: 'realname', sortable: true },
+  {
+    title: 'Name Uppercased',
+    prop: 'realnameuppercase',
+    cell: row => row.realname.toUpperCase()
+  },
+  { title: 'Location', prop: 'location' },
+  { title: 'Last Updated', prop: 'date', sortable: true }
+];
+
+const body = Array.from(new Array(57), () => {
+  const rd = (Math.random() * 10).toFixed(1);
+
+  if (rd > 0.5) {
+    return {
+      username: 'i-am-billy',
+      realname: `Billy ${rd}`,
+      location: 'Mars',
+      date: moment()
+        .subtract(1, 'days')
+        .format('Do MMMM YYYY')
+    };
+  }
+
+  return {
+    username: 'john-nhoj',
+    realname: `John ${rd}`,
+    location: 'Saturn',
+    date: moment()
+      .subtract(2, 'days')
+      .format('Do MMMM YYYY')
+  };
+});
+
+const onSortFunction = {
+  date(columnValue) {
+    // Convert the string date format to UTC timestamp
+    // So the table could sort it by number instead of by string
+    return moment(columnValue, 'Do MMMM YYYY').valueOf();
+  }
+};
+
+const customLabels = {
+  first: '<<',
+  last: '>>',
+  prev: '<',
+  next: '>',
+  show: 'Display',
+  entries: 'rows',
+  noResults: 'There is no data to be displayed'
+};
+
+const classes = {
+  theadCol: css`
+    .table-datatable__root & {
+      &.sortable:hover {
+        background: pink;
+      }
+    }
+  `
+};
+
+storiesOf(categoryName, module).add('Custom classes', () => (
+  <Datatable
+    tableHeaders={header}
+    tableBody={body}
+    tableClass="striped hover responsive"
+    rowsPerPage={5}
+    rowsPerPageOption={[5, 10, 15, 20]}
+    initialSort={{ prop: 'username', isAscending: true }}
+    onSort={onSortFunction}
+    labels={customLabels}
+    classes={classes}
+  />
+));
