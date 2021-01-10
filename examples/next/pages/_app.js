282bedbc0367d94e53cea7e1f54edef370af5769
Try Ajitiono <ballinst@gmail.com>
2021-01-10 09:45:33 +0700

    fix issue
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- examples/next/pages/_app.js
+++ examples/next/pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
