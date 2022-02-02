bcde78643694597108bd42685161f39707055a76
Try Ajitiono <ballinst@gmail.com>
2022-02-02 16:47:42 +0700

    use export type
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,30 @@
-export {
-  DatatableWrapper,
-  DatatableWrapperProps
-} from './components/DatatableWrapper';
-export { Filter, FilterClasses, FilterProps } from './components/Filter';
-export {
-  Pagination,
+export { DatatableWrapper } from './components/DatatableWrapper';
+export type { DatatableWrapperProps } from './components/DatatableWrapper';
+
+export { Filter } from './components/Filter';
+export type { FilterClasses, FilterProps } from './components/Filter';
+
+export { Pagination } from './components/Pagination';
+export type {
   PaginationClasses,
   PaginationLabels,
   PaginationProps
 } from './components/Pagination';
-export {
-  PaginationOpts,
+
+export { PaginationOpts } from './components/PaginationOpts';
+export type {
   PaginationOptsProps,
   PaginationOptionsClasses,
   PaginationOptionsLabels
 } from './components/PaginationOpts';
-export { TableBody, TableBodyProps } from './components/TableBody';
-export {
-  TableHeader,
+
+export { TableBody } from './components/TableBody';
+export type { TableBodyProps } from './components/TableBody';
+
+export { TableHeader } from './components/TableHeader';
+export type {
   TableHeaderClasses,
   TableHeaderProps
 } from './components/TableHeader';
 
-export { ColumnProcessObj, SortType } from './helpers/types';
+export type { ColumnProcessObj, SortType } from './helpers/types';
