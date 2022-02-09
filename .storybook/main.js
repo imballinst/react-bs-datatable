7e0320b44ef7917842567a3f793c45ed76ae528f
Try Ajitiono <ballinst@gmail.com>
2022-02-09 12:34:29 +0700

    cp
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- .storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: [
-    '../src/__stories__/*.stories.mdx',
-    '../src/__stories__/*.stories.@(js|jsx|ts|tsx)'
-  ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  framework: '@storybook/react'
-};
