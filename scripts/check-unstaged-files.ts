2363c943ffb1c60f773ab432680e67ba26f3cca4
Try Ajitiono <ballinst@gmail.com>
2022-02-09 11:19:49 +0700

    add scripts more
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- scripts/check-unstaged-files.ts
+++ scripts/check-unstaged-files.ts
@@ -1,6 +1,12 @@
 import { execa } from 'execa';
+import { setFailed } from '@actions/core';
 
 (async () => {
   const result = await execa('git', ['diff']);
   console.log(result.stdout);
+  if (result.stdout && process.env.CI) {
+    setFailed(
+      'There were changes in the `api` folder. Please re-generate the API docs by doing `yarn typedoc`.'
+    );
+  }
 })();
