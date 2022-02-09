571cf6cc3b5d7171478e8f913e0cffa0410e6d7f
Try Ajitiono <ballinst@gmail.com>
2022-02-09 11:21:57 +0700

    finish script
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- scripts/check-unstaged-files.ts
+++ scripts/check-unstaged-files.ts
@@ -2,9 +2,11 @@ import { execa } from 'execa';
 import { setFailed } from '@actions/core';
 
 (async () => {
-  const result = await execa('git', ['diff']);
-  console.log(result.stdout);
-  if (result.stdout && process.env.CI) {
+  const { stdout } = await execa('git', ['diff', '--name-only']);
+  const arrayOfDirs = stdout.split('\n');
+  const hasApiDocsChanges = arrayOfDirs.find((dir) => dir.startsWith('api'));
+
+  if (hasApiDocsChanges && process.env.CI) {
     setFailed(
       'There were changes in the `api` folder. Please re-generate the API docs by doing `yarn typedoc`.'
     );
