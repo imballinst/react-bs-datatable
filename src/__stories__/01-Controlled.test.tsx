cfac2ee27f12d5a37ce2339edbf837d4cb1ff5ac
Try Ajitiono <ballinst@gmail.com>
2022-05-03 17:48:45 +0700

    ok

--- src/__stories__/01-Controlled.test.tsx
+++ src/__stories__/01-Controlled.test.tsx
@@ -127,16 +127,123 @@ test('Normal use case', async () => {
 });
 
 describe('Composed table row', () => {
-  test('filter, sort, pagination', () => {});
+  test('filter, sort, pagination', async () => {
+    const {
+      getByText,
+      getByRole,
+      getByLabelText,
+      queryByText,
+      getByPlaceholderText
+    } = render(
+      <ControlledComposedTableRow fetchFn={fetchControlledMockData} />
+    );
 
-  test('Custom row on click', () => {
+    await waitForElementToBeRemoved(() =>
+      queryByText('No results to be shown.')
+    );
+
+    let firstBtnElement = getByText('First');
+    let lastBtnElement = getByText('Last');
+    let tableElement = getByRole('table');
+
+    expect(firstBtnElement).toBeDisabled();
+    expect(lastBtnElement).toBeInTheDocument();
+
+    // Test go to the last pagination.
+    fireEvent.click(lastBtnElement);
+    await act(async () => await sleep(200));
+
+    await waitFor(() => expect(firstBtnElement).not.toBeDisabled());
+    await waitFor(() => expect(lastBtnElement).toBeDisabled());
+
+    // With 8 rows per page, the last page should only have 4 rows.
+    expect(
+      tableElement.querySelector('tbody')?.querySelectorAll('tr').length
+    ).toBe(4);
+
+    // Try filtering. It should go back to the first page.
+    let filterElement = getByPlaceholderText('Enter text...');
+    fireEvent.change(filterElement, { target: { value: 'aaren' } });
+    await act(async () => await sleep(200));
+
+    await waitFor(() => expect(firstBtnElement).toBeDisabled());
+
+    let firstNumButtonElement = queryByText('1');
+    let secondNumButtonElement = queryByText('2');
+    let thirdNumButtonElement = queryByText('3');
+    firstBtnElement = getByText('First');
+    lastBtnElement = getByText('Last');
+
+    // All buttons should be disabled, and the "2" and "3" page button
+    // should not exist in the table.
+    expect(firstBtnElement).toBeDisabled();
+    expect(lastBtnElement).toBeDisabled();
+    expect(firstNumButtonElement).toBeDisabled();
+    expect(secondNumButtonElement).not.toBeInTheDocument();
+    expect(thirdNumButtonElement).not.toBeInTheDocument();
+
+    expect(
+      tableElement.querySelector('tbody')?.querySelectorAll('tr').length
+    ).toBe(1);
+
+    // Reset the filter first by clicking the "X" clear filter button.
+    let clearFilterButton = getByLabelText('Clear filter');
+    fireEvent.click(clearFilterButton);
+    await act(async () => await sleep(200));
+
+    firstNumButtonElement = queryByText('1');
+    secondNumButtonElement = queryByText('2');
+    thirdNumButtonElement = queryByText('3');
+    firstBtnElement = getByText('First');
+    lastBtnElement = getByText('Last');
+
+    expect(firstBtnElement).toBeDisabled();
+    expect(lastBtnElement).not.toBeDisabled();
+    expect(firstNumButtonElement).toBeDisabled();
+    expect(secondNumButtonElement).not.toBeDisabled();
+    expect(thirdNumButtonElement).not.toBeDisabled();
+
+    expect(
+      tableElement.querySelector('tbody')?.querySelectorAll('tr').length
+    ).toBe(8);
+
+    // Try sorting.
+    // Sort descending the first column (since it's the initial state).
+    let nameTh = getByText('Name', { selector: 'th' });
+    fireEvent.click(nameTh);
+    await act(async () => await sleep(200));
+
+    let tableRows = tableElement.querySelector('tbody')?.querySelectorAll('tr');
+    expect(tableRows).toBeDefined();
+    expect(tableRows?.[0].querySelector('td')?.textContent).toBe('Wileen');
+    expect(nameTh.getAttribute('data-sort-order')).toBe('desc');
+
+    // Try sorting the other columns.
+    let usernameTh = getByText('Username', { selector: 'th' });
+    fireEvent.click(usernameTh);
+    await act(async () => await sleep(200));
+
+    // The clicked header should have its sort state.
+    tableRows = tableElement.querySelector('tbody')?.querySelectorAll('tr');
+    expect(tableRows).toBeDefined();
+    expect(tableRows?.[0].querySelector('td')?.textContent).toBe('Aaren');
+    expect(usernameTh.getAttribute('data-sort-order')).toBe('asc');
+    // Meanwhile, the "Name" header should reset to its default state.
+    expect(nameTh.getAttribute('data-sort-order')).toBe(null);
+  });
+
+  test('Custom row on click', async () => {
     const clickFn = jest.fn((name) => name);
 
-    const { getByRole } = render(
+    const { getByRole, queryByText } = render(
       <ControlledComposedTableRow
         fetchFn={fetchControlledMockData}
         rowOnClickFn={clickFn}
       />
+    );
+
+    await waitForElementToBeRemoved(() =>
+      queryByText('No results to be shown.')
     );
 
     const tableElement = getByRole('table');
