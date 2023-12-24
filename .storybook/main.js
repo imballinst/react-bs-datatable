4ef3dd267f051d20f510874ac555ebe0ca2ceb22
Try Ajitiono <ballinst@gmail.com>
2023-12-24 02:47:31 +0000

    migrate etce tc

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,9 +3,19 @@ module.exports = {
     '../src/__stories__/*.stories.mdx',
     '../src/__stories__/*.stories.@(js|jsx|ts|tsx)'
   ],
+
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  framework: '@storybook/react',
+
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+
   typescript: {
     reactDocgen: false
+  },
+
+  docs: {
+    autodocs: true
   }
 };
